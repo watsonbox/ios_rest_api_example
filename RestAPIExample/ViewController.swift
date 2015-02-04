a7410036eef0f2c36529ade0b9af37dc9673f968
Howard Wilson <howard@watsonbox.net>
2015-02-04 19:35:12 +0100

    Fetch and print repository in UI

--- RestAPIExample/ViewController.swift
+++ RestAPIExample/ViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+  @IBOutlet weak var textView: UITextView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+
+    Repository.get("watsonbox/pocketsphinx-ruby") { repository in
+      self.textView.text = repository?.description
     }
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 }
-
