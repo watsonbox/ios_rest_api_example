f64676d696133dd15009974619c9e8d0ca5bcbd1
Howard Wilson <howard@watsonbox.net>
2015-02-04 19:28:16 +0100

    Make Repository Printable

--- RestAPIExample/GithubAPI/Models/Repository.swift
+++ RestAPIExample/GithubAPI/Models/Repository.swift
@@ -6,7 +6,7 @@
 //  Copyright (c) 2015 Howard Wilson. All rights reserved.
 //
 
-class Repository : ResponseObjectSerializable {
+class Repository: ResponseObjectSerializable {
   let id: Int
   var name: String
   var fullName: String
@@ -17,7 +17,22 @@ class Repository : ResponseObjectSerializable {
     fullName = data["full_name"].string!
   }
 
+  func toJSON() -> JSON {
+    var json = JSON([
+      "id": id,
+      "name": name,
+      "full_name": fullName
+    ])
+
+    return json
+  }
+
   class func get(name: String, result: (Repository?) -> (), failure: GithubAPIFailureHandler? = nil) {
     GithubAPIClient.sharedInstance.getSingleObject(path: "/repos/\(name)", result: result, failure: failure)
   }
+}
+
+// MARK: - Printable
+extension Repository: Printable {
+  var description: String { return "Repository(" + toJSON().description + ")" }
 }
